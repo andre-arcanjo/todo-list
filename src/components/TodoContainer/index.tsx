78bb6ad5c61e31f008523b684ed87af68c16a5b9
andre-arcanjo <andrearcanjosouza@hotmail.com>
2025-11-30 22:38:56 -0300

    fix: correção de estilização todoContainer para tarefas não sairem pra fora do background

--- src/components/TodoContainer/index.tsx
+++ src/components/TodoContainer/index.tsx
@@ -11,11 +11,10 @@ const TodoContainer = ({children}: TodoContainerProps) => {
     const {theme} = useContext(ThemeContext);
 
     return (
-        <main className={`h-screen ${themeConfig[theme].layout.backgroundColor}`
+        <main className={`min-h-screen ${themeConfig[theme].layout.backgroundColor}`
         }>
-            <div className={`${themeConfig[theme].layout.heroClass} h-80 bg-cover bg-center`}>
-                <div className="max-w-[43.75rem] m-auto p-8">{children}</div>
-            </div>
+            <div className={`${themeConfig[theme].layout.heroClass} h-80 bg-cover bg-center`}></div>
+            <div className="max-w-[43.75rem] m-auto px-8 pb-12 -mt-52">{children}</div>
         </main>
     )
 }
