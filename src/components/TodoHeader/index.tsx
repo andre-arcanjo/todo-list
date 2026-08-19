0300038e7c6d402b0d7bed04759ff16dfdf27477
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-08-18 22:40:03 -0300

    fix:remove unnecessary class in TodoHeader component

--- src/components/TodoHeader/index.tsx
+++ src/components/TodoHeader/index.tsx
@@ -7,7 +7,7 @@ const TodoHeader = () => {
 
   return (
     <header className="flex justify-between p-5 items-center mb-6">
-      <h1 className="text-white text-4xl sm:text text-[2.5rem] font-bold tracking-[1rem]">
+      <h1 className="text-white text-[2.5rem] font-bold tracking-[1rem]">
         TODO
       </h1>
 
